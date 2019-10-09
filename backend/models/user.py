b8be2174769931b488a35bd0db4c7f65217654bb
Anthony Zhong <anthonyzhong96@gmail.com>
2019-10-09 20:32:31 +1100

    Add methods required by flask_login to User model.

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,34 @@
-class User:
-    def __init__(self):
-        #TODO: initialise User object
-        pass
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User():
+    def __init__(self, user_id: str):
+        self._user_id = user_id
+        self._password = None
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+    def get_id(self) -> str:
+        return self.user_id
+
+    @property
+    def user_id(self):
+        return self._user_id
+
+    @property
+    def password(self) -> str:
+        return self._password
+
+    @password.setter
+    def password(self, password: str):
+        self._password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password, password)
