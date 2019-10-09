b379e10d8fbb50efdea4880a361b91ae4d98e9ce
Manlin Wang <z5160017@student.unsw.edu.au>
2019-10-09 22:21:45 +1100

    User table made

--- backend/database/TradiEdb.py
+++ backend/database/TradiEdb.py
@@ -1,22 +1,43 @@
 import sqlite3
+from backend.models.user import User
+
+# connecting to the database
+def connect_db():
+    conn = sqlite3.connect('TradiE.db')
+    c = conn.cursor()
+    objects = []
+    objects.append(conn)
+    objects.append(c)
+    return objects
 
 
+# creating tables for the database
+def init_tables(conn, c):
+    c.execute("drop table if exists USERS")
+
+    c.execute('''CREATE TABLE USERS (
+                userID INTEGER PRIMARY KEY AUTOINCREMENT,
+                lastName TEXT, 
+                firstName TEXT,
+                userName TEXT, 
+                email TEXT,
+                passwordHash TEXT)
+            ''')
+    conn.commit()
+
+def setup_db():
+    db = connect_db()
+    conn = db[0]
+    c = db[1]
+    init_tables(conn, c)
+
+    c.execute("INSERT INTO USERS VALUES (null, 'smith', 'jane', 'janesmith', 'janesmith@gmail.com', 'qwert')")
+    c.execute("Select * from USERS where lastName='smith'")
+    print(c.fetchall())
+    conn.commit()
+
+    conn.close()
 
 
-database = sqlite3.connect('TradiE.db')
-db = database.cursor()
-
-#create table - USERS
-
-def createdb() :
-    if db.fetch()[0] != 1:
-
-        db.execute('''CREATE TABLE USERS (
-            [generated_id] INTEGER PRIMARY KEY, 
-            [lastName] TEXT, [firstName] TEXT, 
-            [userName] TEXT, [email] TEXT, 
-            [passwordHash] TEXT)''')
-
-
-        database.commit()
+setup_db()
 
