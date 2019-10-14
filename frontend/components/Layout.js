f53b0e27d001cad2160a4ce3e64df6afdaa07404
Martin Le <martinle.cse@gmail.com>
2019-10-14 16:43:49 +1100

    removed atlaskit

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import Meta from './Meta';
-import Navigation from './Navigation';
+import Header from './Header';
 import theme from './styles/Theme';
-import '@atlaskit/css-reset';
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
 
 const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
@@ -12,17 +16,52 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
+// just need to call this in order to apply global styles
+// Note. props.theme does not get passed to injectGlobal
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black}
+  }
+  button {
+    font-family: 'radnika_next';
+  }
+`;
+
 class Layout extends Component {
   render() {
     const { children } = this.props;
     return (
       <ThemeProvider theme={theme}>
-        <div>
+        <StyledPage>
           <Meta />
-          <Navigation>
-            <Inner>{children}</Inner>
-          </Navigation>
-        </div>
+          <Header />
+          <Inner>{children}</Inner>
+        </StyledPage>
+        <GlobalStyle />
       </ThemeProvider>
     );
   }
