3f363c71875494ae26423dd7dec88dffe8cafda9
Martin Le <martinle.cse@gmail.com>
2019-10-18 23:14:38 +1100

    added chart js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import React, { Fragment, Component } from 'react';
-import logoTurqoise from '../static/logos/logo-turquoise.png';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -14,160 +12,81 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
+import logoTurqoise from '../static/logos/logo-turquoise.png';
 
 class Login extends Component {
-
   componentDidMount() {
-    //need login
+    // need login
   }
 
-  render(){
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          flexDirection: 'column',
+          height: '100%',
+          backgroundColor: 'DarkTurquoise',
+          justifyContent: 'center',
+        }}
+      >
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div>
+            <img src={logoTurqoise} alt="Logo" />
+            <form noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
 
-  return (
-
-    <div
-      style={{
-        display: 'flex',
-        width: '100%',
-        flexDirection: 'column',
-        height: '100%',
-        backgroundColor: 'DarkTurquoise',
-        justifyContent: 'center',
-
-      }}
-     >
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div >
-        <img src={logoTurqoise} alt="Logo" />
-        <form  noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Don't have an account? Sign Up
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Container>
       </div>
-    </Container>
-   </div>
-
-  );
+    );
   }
-
 }
 
-
-Login.propTypes = {
-};
+Login.propTypes = {};
 
 export default Login;
-=======
-import React, { Fragment } from 'react';
-import TextField from '@atlaskit/textfield';
-import Button, { ButtonGroup } from '@atlaskit/button';
-import { Checkbox } from '@atlaskit/checkbox';
-import Form, {
-  CheckboxField,
-  Field,
-  FormFooter,
-  HelperMessage,
-  ErrorMessage,
-  ValidMessage,
-} from '@atlaskit/form';
-
-export default  () => (
-  <div
-    style={{
-      display: 'flex',
-      width: '400px',
-      maxWidth: '100%',
-      margin: '0 auto',
-      flexDirection: 'column',
-    }}
-  >
-    <header>Tradie</header>
-    <Form
-      onSubmit={data => {
-        console.log('form data', data);
-        return new Promise(resolve => setTimeout(resolve, 2000)).then(() =>
-          data.username === 'error' ? { username: 'IN_USE' } : undefined,
-        );
-      }}
-    >
-      {({ formProps, submitting }) => (
-        <form {...formProps}>
-          <Field name="username" label="User name" isRequired defaultValue="">
-            {({ fieldProps, error }) => (
-              <Fragment>
-                <TextField autoComplete="off" {...fieldProps} />
-              </Fragment>
-            )}
-          </Field>
-          <Field
-            name="password"
-            label="Password"
-            defaultValue=""
-            isRequired
-          >
-            {({ fieldProps, error, valid }) => (
-              <Fragment>
-                <TextField type="password" {...fieldProps} />
-              </Fragment>
-            )}
-          </Field>
-          <FormFooter>
-            <ButtonGroup>
-              <Button type="submit" appearance="primary" isLoading={submitting}>
-                Login
-              </Button>
-            </ButtonGroup>
-          </FormFooter>
-        </form>
-      )}
-    </Form>
-  </div>
-);
-
-
->>>>>>> 6dac774a96fc6be9edec2aa10824aa36e443385c
