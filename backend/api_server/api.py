cdba732827f7f2ba52ca3ee49cd16aeebbd94f4c
Anthony Zhong <anthonyzhong96@gmail.com>
2019-10-19 23:09:19 +1100

    Move login endpoints into login module.

--- backend/api_server/api.py
+++ backend/api_server/api.py
@@ -1,13 +1,8 @@
-from exception.user.user_already_exists_error import UserAlreadyExistsError
 from flask import (
     Blueprint, request, jsonify
 )
-from flask_login import login_user
-from login.user_manager import UserManager
 
 # IMPORT FUNCTIONS HERE
-user_manager = UserManager()
-
 bp = Blueprint('api', __name__, url_prefix='/api/v1')
 
 # NEW ROUTES HERE
@@ -23,33 +18,3 @@ def index():
             'MSFT'
         ]
     })
-
-# Login Routes
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return "Insert frontend form to login here"
-    elif request.method == 'POST':
-        user_id = request.form.get('user_id')
-        password = request.form.get('password')
-        user = user_manager.get_user(user_id)
-        if user:
-            if user.check_password(password):
-                login_user(user)
-                return "Login successful"
-
-        return "Login failed"
-
-@bp.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'GET':
-        return "Insert frontend form to signup here"
-    elif request.method =='POST':
-        user_id = request.form.get('user_id')
-        password = request.form.get('password')
-        try:
-            user_manager.create_new_user(user_id, password)
-        except UserAlreadyExistsError:
-            return "Username unavailable"
-
-        return "Signed up"
---
+++ backend/login/api.py
@@ -0,0 +1,38 @@
+from exception.user.user_already_exists_error import UserAlreadyExistsError
+from flask import (
+    Blueprint, request, Response
+)
+from flask_login import login_user
+from login.user_manager import UserManager
+
+bp = Blueprint('login', __name__, url_prefix='/login')
+
+user_manager = UserManager()
+
+# Login Routes
+@bp.route('/', methods=['POST'])
+def login():
+    user_id = request.form.get('user_id')
+    password = request.form.get('password')
+    user = user_manager.get_user(user_id)
+    if user:
+        if user.check_password(password):
+            login_user(user)
+            return Response(response="Login successful.", status=200)
+
+    return Response(response="Authentication failed.", status=401)
+
+
+@bp.route('/register', methods=['POST'])
+def signup():
+    user_id = request.form.get('user_id')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user_manager.create_new_user(user_id, first_name, last_name, email, password)
+    except UserAlreadyExistsError:
+        return Response(response="Username unavailable.", status=409)
+
+    return Response(response="Registration successful.", status=201)
