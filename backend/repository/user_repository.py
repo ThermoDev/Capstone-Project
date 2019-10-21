7254153a442c62dd057459b91b91a3154cd9d719
Anthony Zhong <anthonyzhong96@gmail.com>
2019-10-21 14:47:08 +1100

    Refactor UserRepository to execute SQL queries securely.

--- backend/repository/user_repository.py
+++ backend/repository/user_repository.py
@@ -11,7 +11,7 @@ class UserRepository:
     def get_user(self, user_id: str) -> User:
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            output = cursor.execute(f'SELECT * FROM User WHERE username="{user_id}"')
+            output = cursor.execute('SELECT * FROM User WHERE username=?', (user_id, ))
             result = output.fetchone()
             if not result:
                 raise UserNotFoundError(user_id)
@@ -21,7 +21,7 @@ class UserRepository:
     def has_user(self, user_id: str) -> bool:
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            output = cursor.execute(f'SELECT * FROM User WHERE username="{user_id}"')
+            output = cursor.execute('SELECT * FROM User WHERE username=?', (user_id, ))
             result = output.fetchone()
 
             return bool(result)
@@ -29,23 +29,16 @@ class UserRepository:
     def add_user(self, user: User):
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            cursor.execute(f'INSERT INTO User VALUES ({self._unpack_user_for_insert(user)})')
+            cursor.execute('INSERT INTO User VALUES (?, ?, ?, ?, ?)', self._unpack_user(user))
             connection.commit()
 
     def update_user(self, user: User):
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            cursor.execute(f'UPDATE User Set {self._unpack_user_for_update(user)}')
+            cursor.execute('UPDATE User SET user_id=?, first_name=?, last_name=?, email=?, password=?',
+                           self._unpack_user(user))
             connection.commit()
 
     @staticmethod
-    def _unpack_user_for_insert(user: User) -> str:
-        return f'"{user.user_id}", "{user.first_name}", "{user.last_name}", "{user.email}", "{user.password}"'
-
-    @staticmethod
-    def _unpack_user_for_update(user: User) -> str:
-        return (f'user_id="{user.user_id}", '
-                f'first_name="{user.first_name}", '
-                f'last_name="{user.last_name}", '
-                f'email="{user.email}", '
-                f'password="{user.password}"')
+    def _unpack_user(user: User) -> tuple:
+        return user.user_id, user.first_name, user.last_name, user.email, user.password
