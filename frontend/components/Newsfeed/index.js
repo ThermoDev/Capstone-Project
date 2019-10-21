c8432fdb973a82bd10d63ad45f578a06346772b8
Martin Le <martinle.cse@gmail.com>
2019-10-21 23:51:58 +1100

    intial styling

---
+++ frontend/components/Newsfeed/index.js
@@ -0,0 +1,32 @@
+// import { Grid, Paper } from '@material-ui/core';
+// import AnnouncementIcon from '@material-ui/icons/Announcement';
+
+// const NewsItem = props => {
+//   const { primary, secondary } = props;
+//   return (
+//     <Paper>
+//       <Grid container spacing={2}>
+//         <Grid item>
+//           <AnnouncementIcon />
+//         </Grid>
+//         <Grid item xs>
+//           {primary}
+//         </Grid>
+//         <Grid item xs>
+//           {secondary}
+//         </Grid>
+//       </Grid>
+//     </Paper>
+//   );
+// };
+
+// const Newsfeed = props => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <NewsItem primary="initial news" secondary="more details to come" />
+//     </div>
+//   );
+// };
+
+// export default Newsfeed;
--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import Container from '@material-ui/core/Container';
+import { Container, Grid, useMediaQuery } from '@material-ui/core';
+import { Line } from '../components/Graph';
 
 const ColorBox = styled.div`
-    background-color: ${props => props.bgColor || 'black'}
-    font-family: Montserrat;
-    font-weight: bold;
-    color: white;
-    text-align: center;
-    min-height: 5rem;
-    padding: 2rem 0;
-  `;
+  background-color: orangered;
+  font-family: Montserrat;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+  min-height: 5rem;
+  padding: 2rem 0;
+`;
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <p>Dashboard</p>
-        <Container maxWidth="md">
-          <ColorBox>Container</ColorBox>
-        </Container>
-      </div>
-    );
-  }
-}
+const Dashboard = () => {
+  const isSmall = useMediaQuery('(max-width: 600px)');
+  console.log(isSmall);
+  return (
+    <div>
+      <Container maxWidth="lg">
+        <Grid container spacing={3} direction={isSmall ? 'column' : 'row'}>
+          <Grid item xs={12}>
+            <Line />
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <ColorBox>Container</ColorBox>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
+              enim soluta et deleniti placeat eum, cupiditate similique
+              reiciendis explicabo doloribus omnis porro laboriosam, eaque
+              molestias. Corporis optio nostrum facere vero?
+            </p>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default Dashboard;
