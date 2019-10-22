7372c2773fb8972f67ce69bcdf856631ab24809a
Anthony Zhong <anthonyzhong96@gmail.com>
2019-10-22 15:29:34 +1100

    UserRepository subclasses BaseRepository.

--- backend/repository/user_repository.py
+++ backend/repository/user_repository.py
@@ -4,15 +4,30 @@ import os
 
 import sqlite3
 
+from repository.base_respository import BaseRepository
 
-class UserRepository:
+
+class UsersTable:
+    TABLE_NAME = 'Users'
+
+    class Columns:
+        USERNAME = 'username'
+        FIRST_NAME = 'firstName'
+        LAST_NAME = 'lastName'
+        EMAIL = 'email'
+        PASSWORD = 'password'
+
+
+class UserRepository(BaseRepository):
     def __init__(self):
         self._db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources/TradiE.db')
 
     def get_user(self, user_id: str) -> User:
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            output = cursor.execute('SELECT * FROM Users WHERE username=?', (user_id, ))
+            query = self.build_select_all_query(table=UsersTable.TABLE_NAME,
+                                                identifiers=(UsersTable.Columns.USERNAME, ))
+            output = cursor.execute(query, (user_id, ))
             result = output.fetchone()
             if not result:
                 raise UserNotFoundError(user_id)
@@ -22,7 +37,9 @@ class UserRepository:
     def has_user(self, user_id: str) -> bool:
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            output = cursor.execute('SELECT * FROM Users WHERE username=?', (user_id, ))
+            query = self.build_select_all_query(table=UsersTable.TABLE_NAME,
+                                                identifiers=(UsersTable.Columns.USERNAME, ))
+            output = cursor.execute(query, (user_id, ))
             result = output.fetchone()
 
             return bool(result)
@@ -30,15 +47,29 @@ class UserRepository:
     def add_user(self, user: User):
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            cursor.execute('INSERT INTO Users (username, firstName, lastName, email, password) VALUES (?, ?, ?, ?, ?)',
-                           self._unpack_user(user))
+            query = self.build_insert_query(table=UsersTable.TABLE_NAME,
+                                            columns=(
+                                                UsersTable.Columns.USERNAME,
+                                                UsersTable.Columns.FIRST_NAME,
+                                                UsersTable.Columns.LAST_NAME,
+                                                UsersTable.Columns.EMAIL,
+                                                UsersTable.Columns.PASSWORD,
+                                            ))
+            cursor.execute(query, self._unpack_user(user))
             connection.commit()
 
     def update_user(self, user: User):
         with sqlite3.connect(self._db_path) as connection:
             cursor = connection.cursor()
-            cursor.execute('UPDATE User SET username=?, firstName=?, lastName=?, email=?, password=?',
-                           self._unpack_user(user))
+            query = self.build_update_query(table=UsersTable.TABLE_NAME,
+                                            columns=(
+                                                UsersTable.Columns.USERNAME,
+                                                UsersTable.Columns.FIRST_NAME,
+                                                UsersTable.Columns.LAST_NAME,
+                                                UsersTable.Columns.EMAIL,
+                                                UsersTable.Columns.PASSWORD,
+                                            ))
+            cursor.execute(query, self._unpack_user(user))
             connection.commit()
 
     @staticmethod
