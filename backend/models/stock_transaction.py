52a698fa0d1a42fcc313c3b24ed8f870bbfd15ac
Anthony Zhong <anthonyzhong96@gmail.com>
2019-10-22 15:11:13 +1100

    Update Portfolio and StockTransaction models.

--- backend/models/stock_transaction.py
+++ backend/models/stock_transaction.py
@@ -1,4 +1,5 @@
-from backend.models.stock import Stock
+from datetime import datetime
+from typing import Optional
 
 
 class StockTransaction:
@@ -8,14 +9,53 @@ class StockTransaction:
     ...
     Attributes
     ----------
-    stock: `Stock`
-        The stock associated with the transaction
-    volume: `int`
-        The volume of the stock bought or sold - can be negative but must be non-zero
+    transaction_id: `int`
+        The unique id key of the transaction
+    portfolio_id: `int`
+        The portfolio id that transaction belongs to
+    company_code: `str`
+        The company code of the stock
     price: `int`
         The price at which the stock was bought or sold
+    volume: `int`
+        The volume of the stock bought or sold - can be negative but must be non-zero
+    transaction_time: `datetime`
+        The datetime of the transaction
     """
-    def __init__(self, stock, volume, price):
-        self.stock = stock
-        self.volume = volume
-        self.price = price
+    def __init__(self,
+                 transaction_id: Optional[int],
+                 portfolio_id: int,
+                 company_code: str,
+                 price: float,
+                 volume: int,
+                 transaction_time: datetime):
+        self._transaction_id = transaction_id
+        self._portfolio_id = portfolio_id
+        self._company_code = company_code
+        self._price = price
+        self._volume = volume
+        self._transaction_time = transaction_time
+
+    @property
+    def transaction_id(self) -> int:
+        return self._transaction_id
+
+    @property
+    def portfolio_id(self) -> int:
+        return self._portfolio_id
+
+    @property
+    def company_code(self) -> str:
+        return self._company_code
+
+    @property
+    def price(self) -> float:
+        return self._price
+
+    @property
+    def volume(self) -> int:
+        return self._volume
+
+    @property
+    def transaction_time(self) -> datetime:
+        return self._transaction_time
