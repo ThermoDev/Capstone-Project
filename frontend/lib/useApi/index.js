38251153d72192660e2fd47dca3bed4769b158fc
Martin Le <martinle.cse@gmail.com>
2019-10-30 16:39:25 +1100

    stock api implemented

--- frontend/lib/useApi/index.js
+++ frontend/lib/useApi/index.js
@@ -38,10 +38,126 @@ export default () => {
     };
   };
 
+  const getStockApi = () => {
+    const [state, setUrl] = useApi('stock');
+    const getStock = name => {
+      setUrl(`stock?symbol=${name}`);
+    };
+    return {
+      stock: state,
+      getStock,
+    };
+  };
+
+  const getMarketPriceApi = () => {
+    const [state, setUrl] = useApi('stock/price');
+    const getCurrentMarketPrice = symbol => {
+      setUrl(`stock/price?symbol=${symbol}`);
+    };
+    return {
+      marketPrice: state,
+      getCurrentMarketPrice,
+    };
+  };
+
+  const getPercentChangeApi = () => {
+    const [state, setUrl] = useApi('stock/pctchange');
+    const getPercentChange = symbol => {
+      setUrl(`stock/pctchange?symbol=${symbol}`);
+    };
+    return {
+      percent: state,
+      getPercentChange,
+    };
+  };
+
+  const getDollarChangeApi = () => {
+    const [state, setUrl] = useApi('stock/dollarchange');
+    const getDollarChange = symbol => {
+      setUrl(`stock/dollarchange?symbol=${symbol}`);
+    };
+    return {
+      dollarChange: state,
+      getDollarChange,
+    };
+  };
+
+  const getYtdApi = () => {
+    const [state, setUrl] = useApi('/stock/ytd');
+    const getYtd = symbol => {
+      setUrl(`/stock/ytd?${symbol}`);
+    };
+    return {
+      ytd: state,
+      getYtd,
+    };
+  };
+
+  const getAllSymbolsApi = () => {
+    const [state, setUrl] = useApi('/stock/getallsymbols/');
+    const getAllSymbols = () => setUrl('/stock/getallsymbols/');
+    return {
+      symbols: state,
+      getAllSymbols,
+    };
+  };
+
+  const searchStockApi = () => {
+    const [state, setUrl] = useApi('stock/search/');
+    const searchStock = query => {
+      setUrl(`stock/search/${query}`);
+    };
+    return {
+      searchResults: state,
+      searchStock,
+    };
+  };
+
+  const infoApi = () => {
+    const [state, setUrl] = useApi('stock/info/');
+    const getInfo = symbol => {
+      setUrl(`stock/info/${symbol}`);
+    };
+    return {
+      info: state,
+      getInfo,
+    };
+  };
+
+  const getIndustryApi = () => {
+    const [state, setUrl] = useApi('stock/industry/');
+    const getIndustry = symbol => {
+      setUrl(`stock/industry/${symbol}`);
+    };
+    return {
+      industry: state,
+      getIndustry,
+    };
+  };
+
+  const getRandomStocksApi = () => {
+    const [state, setUrl] = useApi('stock/random');
+    const getRandom = () => setUrl('stock/random');
+    return {
+      result: state,
+      getRandom,
+    };
+  };
+
   return {
     portfoliosApi,
     portfolioApi,
     createPortfolioApi,
     processTransactionApi,
+    getStockApi,
+    getMarketPriceApi,
+    getPercentChangeApi,
+    getDollarChangeApi,
+    getYtdApi,
+    getAllSymbolsApi,
+    searchStockApi,
+    infoApi,
+    getIndustryApi,
+    getRandomStocksApi,
   };
 };
