570560f4297d545e56b75d041894c5ee3d6188ae
Manlin Wang <z5160017@student.unsw.edu.au>
2019-11-02 23:31:00 +1100

    test port repo has errors

--- backend/tests/test_portfolio_repository.py
+++ backend/tests/test_portfolio_repository.py
@@ -1,6 +1,7 @@
 import sqlite3
 import unittest
 
+from models.portfolio import Portfolio
 from repository.portfolio_repository import PortfolioRepository
 from werkzeug.security import generate_password_hash
 
@@ -14,9 +15,11 @@ class PortfolioRepositoryTests(unittest.TestCase):
     portfolio_repository = TestPortfolioRepository()
 
     def test_get_all_portfolios_for_user(self):
-        pass
 
-    def get_portfolio_by_id(self):
+        portfolio = self.portfolio_repository.get_all_portfolios_for_user('test')
+        
+
+    def test_get_portfolio_by_id(self):
         portfolio = self.portfolio_repository.get_portfolio_by_id(1)
 
         self.assertEqual(portfolio.portfolio_id, 1)
@@ -28,13 +31,32 @@ class PortfolioRepositoryTests(unittest.TestCase):
         pass
 
     def test_has_portfolio_by_user_and_name(self):
-        pass
+
+        portfolio = self.portfolio_repository.has_portfolio_by_user_and_name('test', 'myFirstPort')
+
+        self.assertEqual(portfolio, True)
 
     def test_add_portfolio(self):
-        pass
+
+        portfolio = Portfolio(None, 'test', 'myThirdPort', 15000.00, [])
+
+        portfolio = self.portfolio_repository.add_portfolio(portfolio)
+        portfolio = self.portfolio_repository.get_portfolio_by_id(portfolio.portfolio_id)
+
+        self.assertEqual(portfolio.portfolio_id, 3)
+        self.assertEqual(portfolio.holder, 'test')
+        self.assertEqual(portfolio.name, 'myThirdPort')
+        self.assertEqual(portfolio.cash, 15000.00)
 
     def test_update_portfolio(self):
-        pass
+
+        portfolio = Portfolio(1, 'test', 'myFirstPort', 200000.00, [])
+        portfolio = self.portfolio_repository.update_portfolio(portfolio)
+
+        self.assertEqual(portfolio.portfolio_id, 1)
+        self.assertEqual(portfolio.holder, 'test')
+        self.assertEqual(portfolio.name, 'myFirstPort')
+        self.assertEqual(portfolio.cash, 200000.00)
 
 
 if __name__ == '__main__':
@@ -51,7 +73,7 @@ if __name__ == '__main__':
         cursor.execute('''INSERT INTO Users VALUES (?, ?, ?, ?, ?)''',
                        ('test', 'Martin', 'Le', 'minh.le023@gmail.com', generate_password_hash('test', method='sha256')))
         cursor.execute('''DROP TABLE IF EXISTS Portfolios''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS Portfolios (
+        cursor.execute('''CREATE TABLE Portfolios (
                             portfolioID integer PRIMARY KEY autoincrement,
                             holder varchar(50) references User(username),
                             name text,
