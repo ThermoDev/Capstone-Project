ce1935b61bf5303506f1395a237149f43c174fed
Celine <z5116868@unsw.edu.au>
2019-11-07 21:13:57 +1100

    added text boxes to the fields

--- frontend/pages/settings.js
+++ frontend/pages/settings.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import styled from 'styled-components';
 import Router from 'next/router';
 import { Container, Typography, Card } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import { useAuth } from '../lib/useAuth';
 
 const ColorBox = styled.div`
@@ -19,8 +22,21 @@ const StyledTypography = styled(Typography)`
   align-self: flex-start;
 `;
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
 const Settings = () => {
   const { user, isAuthenticated } = useAuth();
+  const classes = useStyles();
 
   useEffect(() => {
     if (!isAuthenticated()) {
@@ -36,28 +52,141 @@ const Settings = () => {
             <StyledTypography component="h1" variant="h6">
               Settings
             </StyledTypography>
+            <br />
+            <Card style={{ width: '100%', height: '50px' }}>
+              Current Password
+              <form className={classes.container} noValidate autoComplete="off">
+                <div>
+                  <TextField
+                    id="outlined-basic"
+                    className={classes.textField}
+                    label="Outlined"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </div>
+              </form>
+            </Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
             <Card style={{ width: '100%', height: '50px' }}>New Password</Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
           </ColorBox>
           <br />
           <ColorBox>
             <StyledTypography component="h1" variant="h6">
               Change Username
             </StyledTypography>
+            <br />
+            <Card style={{ width: '100%', height: '50px', align: 'center' }}>
+              Current Username
+            </Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
             <Card style={{ width: '100%', height: '50px' }}>New Username</Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
           </ColorBox>
           <br />
           <ColorBox>
             <StyledTypography component="h1" variant="h6">
               Change Password
             </StyledTypography>
+            <br />
+            <Card style={{ width: '100%', height: '50px' }}>
+              Current Password
+            </Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
             <Card style={{ width: '100%', height: '50px' }}>New Password</Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
           </ColorBox>
           <br />
           <ColorBox>
             <StyledTypography component="h1" variant="h6">
               Change Email
             </StyledTypography>
+            <br />
+            <Card style={{ width: '100%', height: '50px' }}>Current Email</Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
             <Card style={{ width: '100%', height: '50px' }}>New Email</Card>
+            <form className={classes.container} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  className={classes.textField}
+                  label="Outlined"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </div>
+            </form>
           </ColorBox>
           <br />
         </Container>
