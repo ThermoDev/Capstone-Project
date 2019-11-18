5507167b8c34ee39dc9043818a5100db42fd607e
Celine <z5116868@unsw.edu.au>
2019-11-18 16:05:47 +1100

    finished settings page

--- frontend/pages/settings.js
+++ frontend/pages/settings.js
@@ -1,10 +1,21 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Router from 'next/router';
-import { Container, Typography, Card } from '@material-ui/core';
 
+import { Container, Typography, Card } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import clsx from 'clsx';
+
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
 import { useAuth } from '../lib/useAuth';
 
 const ColorBox = styled.div`
@@ -24,20 +35,53 @@ const StyledTypography = styled(Typography)`
 `;
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
   container: {
     display: 'flex',
     flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
   },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     width: 200,
   },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
+
+const handleChange = prop => event => {
+  setValues({ ...values, [prop]: event.target.value });
+};
+
+const handleClickShowPassword = () => {
+  setValues({ ...values, showPassword: !values.showPassword });
+};
+
+const handleMouseDownPassword = event => {
+  event.preventDefault();
+};
 
 const Settings = () => {
   const { user, isAuthenticated } = useAuth();
   const classes = useStyles();
+
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
 
   useEffect(() => {
     if (!isAuthenticated()) {
@@ -50,49 +94,13 @@ const Settings = () => {
       {isAuthenticated() && (
         <Container maxWidth="lg">
           <ColorBox>
-            <StyledTypography component="h1" variant="h6">
+            <StyledTypography component="h1" variant="h6" colour="black">
               Settings
             </StyledTypography>
-            <br />
-            <Card style={{ width: '100%', height: '50px' }}>
-              Current Password
-              <form className={classes.container} noValidate autoComplete="off">
-                <div>
-                  <TextField
-                    id="outlined-basic"
-                    className={classes.textField}
-                    label="Current Password"
-                    margin="normal"
-                    variant="outlined"
-                  />
-                </div>
-              </form>
-            </Card>
-            <form className={classes.container} noValidate autoComplete="off">
-              <div>
-                <TextField
-                  id="outlined-basic"
-                  className={classes.textField}
-                  label="Outlined"
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-            </form>
-            <Card style={{ width: '100%', height: '50px' }}>New Password</Card>
-            <form className={classes.container} noValidate autoComplete="off">
-              <div>
-                <TextField
-                  id="outlined-basic"
-                  className={classes.textField}
-                  label="Outlined"
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-            </form>
           </ColorBox>
+
           <br />
+
           <ColorBox>
             <StyledTypography component="h1" variant="h6">
               Change Username
@@ -106,7 +114,7 @@ const Settings = () => {
                 <TextField
                   id="outlined-basic"
                   className={classes.textField}
-                  label="Outlined"
+                  label="Current Username"
                   margin="normal"
                   variant="outlined"
                 />
@@ -118,13 +126,14 @@ const Settings = () => {
                 <TextField
                   id="outlined-basic"
                   className={classes.textField}
-                  label="Outlined"
+                  label="New Username"
                   margin="normal"
                   variant="outlined"
                 />
               </div>
             </form>
           </ColorBox>
+
           <br />
           <ColorBox>
             <StyledTypography component="h1" variant="h6">
@@ -134,31 +143,61 @@ const Settings = () => {
             <Card style={{ width: '100%', height: '50px' }}>
               Current Password
             </Card>
-            <form className={classes.container} noValidate autoComplete="off">
-              <div>
-                <TextField
-                  id="outlined-basic"
-                  className={classes.textField}
-                  label="Outlined"
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-            </form>
+            <FormControl
+              className={clsx(classes.margin, classes.textField)}
+              variant="filled"
+            >
+              <InputLabel htmlFor="filled-adornment-password">
+                Current Password
+              </InputLabel>
+              <FilledInput
+                id="filled-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.password}
+                onChange={handleChange('password')}
+                endAdornment={
+                  <InputAdornment position="right">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
             <Card style={{ width: '100%', height: '50px' }}>New Password</Card>
-            <form className={classes.container} noValidate autoComplete="off">
-              <div>
-                <TextField
-                  id="outlined-basic"
-                  className={classes.textField}
-                  label="Outlined"
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-            </form>
+            <FormControl
+              className={clsx(classes.margin, classes.textField)}
+              variant="filled"
+            >
+              <InputLabel htmlFor="filled-adornment-password">
+                New Password
+              </InputLabel>
+              <FilledInput
+                id="filled-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.password}
+                onChange={handleChange('password')}
+                endAdornment={
+                  <InputAdornment position="right">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </ColorBox>
+
           <br />
+
           <ColorBox>
             <StyledTypography component="h1" variant="h6">
               Change Email
@@ -170,7 +209,7 @@ const Settings = () => {
                 <TextField
                   id="outlined-basic"
                   className={classes.textField}
-                  label="Outlined"
+                  label="Current Email"
                   margin="normal"
                   variant="outlined"
                 />
@@ -182,7 +221,7 @@ const Settings = () => {
                 <TextField
                   id="outlined-basic"
                   className={classes.textField}
-                  label="Outlined"
+                  label="New Email"
                   margin="normal"
                   variant="outlined"
                 />
@@ -190,6 +229,20 @@ const Settings = () => {
             </form>
           </ColorBox>
           <br />
+
+          <ColorBox>
+            <StyledTypography component="h1" variant="h6">
+              Delete Account
+            </StyledTypography>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete Account
+            </Button>
+          </ColorBox>
         </Container>
       )}
     </div>
